d5adcc7a832a1a85a8095e75afdf2714a1dc4d05
Christian B. Knudsen <cbk@kb.dk>
2025-03-12 10:06:45 +0100

    Ups. read_csv rettet til read_csv2

--- kursus-tilmeldinger/read_kursus_tilmeldinger.R
+++ kursus-tilmeldinger/read_kursus_tilmeldinger.R
@@ -184,6 +184,6 @@ tilmeldinger <- tilmeldinger %>%
 
 write_csv2(tilmeldinger, "data/tilmeldingsdata/tilmeldinger.csv")
 write_csv2(presenter, "data/tilmeldingsdata/presenter.csv")
-write_csv(metadata, "data/tilmeldingsdata/metadata.csv")
+write_csv2(metadata, "data/tilmeldingsdata/metadata.csv")
 
 
